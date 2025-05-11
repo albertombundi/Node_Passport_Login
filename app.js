b63ff2d5ee640fab08fd43ec7d974064fe1e9712
albertombundi <albertombundi@outlook.com>
2025-05-11 23:00:41 +0100

    Updates in all files

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// DB Config
+const db = require('./config/keys').MongoURI;
 
+// Connect to MongoDB
+mongoose
+  .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://albertombundi:alberto123@cluster1.bpsjckv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',
+}
