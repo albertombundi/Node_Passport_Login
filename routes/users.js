f0f1e8f1aafcdae4251ae7c243595a1ac68f63fd
albertombundi <albertombundi@outlook.com>
2025-05-12 15:07:07 +0100

    update users.js

--- routes/users.js
+++ routes/users.js
@@ -75,8 +75,6 @@ router.post('/register',(req, res) => {
                         })
                         .catch(err => console.log(err));
 
-
-
                 } ))
             }
         });
