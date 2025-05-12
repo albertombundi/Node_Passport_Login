f1d386c8611557cb533bd1318a14e00d66395bb9
albertombundi <albertombundi@outlook.com>
2025-05-12 19:28:59 +0100

    adicionei a sessão do usuario e os avisos de confimação de registo

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session =  require('express-session');
 
 const app = express();
 
 // DB Config
 const db = require('./config/keys').MongoURI;
 
-// Connect to MongoDB
-mongoose
-  .connect(db, {useNewUrlParser: true})
+// Connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
@@ -17,8 +18,25 @@ mongoose
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// Bodyparser
+// Body parser
 app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//connect flash
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 // Routes
 app.use('/', require('./routes/index'));
